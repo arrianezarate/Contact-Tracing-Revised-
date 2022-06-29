4ee6f7b31243687ea96d92ce62aeba013ef90e0e
arrianezarate <arrianenic.z@gmail.com>
2022-06-29 17:07:07 +0800

    admin access form design done

---
+++ contact tracing revised/admin_access.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contact_tracing_revised
+{
+    public partial class admin_access : Form
+    {
+        public admin_access()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

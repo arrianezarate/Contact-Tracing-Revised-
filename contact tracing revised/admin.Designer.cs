d2890a840b335be239471d2de88496f6df2f04d8
arrianezarate <arrianenic.z@gmail.com>
2022-06-29 14:58:39 +0800

    admin login form w/ design done

---
+++ contact tracing revised/admin.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace contact_tracing_revised
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Admin = new System.Windows.Forms.Label();
+            this.txtbox_Username = new System.Windows.Forms.TextBox();
+            this.txtbox_Password = new System.Windows.Forms.TextBox();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Admin
+            // 
+            this.lbl_Admin.AutoSize = true;
+            this.lbl_Admin.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Admin.ForeColor = System.Drawing.Color.White;
+            this.lbl_Admin.Location = new System.Drawing.Point(85, 81);
+            this.lbl_Admin.Name = "lbl_Admin";
+            this.lbl_Admin.Size = new System.Drawing.Size(267, 60);
+            this.lbl_Admin.TabIndex = 1;
+            this.lbl_Admin.Text = "ADMIN LOGIN";
+            // 
+            // txtbox_Username
+            // 
+            this.txtbox_Username.Location = new System.Drawing.Point(527, 71);
+            this.txtbox_Username.Name = "txtbox_Username";
+            this.txtbox_Username.Size = new System.Drawing.Size(150, 23);
+            this.txtbox_Username.TabIndex = 2;
+            // 
+            // txtbox_Password
+            // 
+            this.txtbox_Password.Location = new System.Drawing.Point(527, 134);
+            this.txtbox_Password.Name = "txtbox_Password";
+            this.txtbox_Password.Size = new System.Drawing.Size(150, 23);
+            this.txtbox_Password.TabIndex = 3;
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Username.ForeColor = System.Drawing.Color.White;
+            this.lbl_Username.Location = new System.Drawing.Point(416, 71);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(75, 18);
+            this.lbl_Username.TabIndex = 4;
+            this.lbl_Username.Text = "Username";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Password.ForeColor = System.Drawing.Color.White;
+            this.lbl_Password.Location = new System.Drawing.Point(419, 134);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(72, 18);
+            this.lbl_Password.TabIndex = 5;
+            this.lbl_Password.Text = "Password";
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(157)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(744, 240);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.txtbox_Password);
+            this.Controls.Add(this.txtbox_Username);
+            this.Controls.Add(this.lbl_Admin);
+            this.Name = "Admin";
+            this.Text = "Admin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbl_Admin;
+        private TextBox txtbox_Username;
+        private TextBox txtbox_Password;
+        private Label lbl_Username;
+        private Label lbl_Password;
+    }
+}
